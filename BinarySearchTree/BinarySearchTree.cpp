7c16f263fd667e9cc2eabdd7bb0646aae903260a
GarryLau <gan.lau@outlook.com>
2017-10-25 09:48:38 +0800

    Update BinarySearchTree.cpp

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -1,5 +1,5 @@
 #include "BinarySearchTree.h"
-
+#include <iostream>
 
 BST::BST()
 {
@@ -7,4 +7,54 @@ BST::BST()
 
 BST::~BST()
 {
+}
+
+/* 访问节点元素 */
+void BST::visit(BSTNode *p) const
+{
+	std::cout << p->value << "  " << std::endl;
+}
+
+/* 深度优先之前序遍历 */
+void BST::preordor() const
+{
+	preordor(root);
+}
+void BST::preordor(BSTNode *p) const
+{
+	if (nullptr != p)
+	{
+		visit(p);
+		preordor(p->left);
+		preordor(p->right);
+	}
+}
+/* 深度优先之中序遍历 */
+void BST::inordor() const
+{
+	inordor(root);
+}
+void BST::inordor(BSTNode *p) const
+{
+	if (nullptr != p)
+	{
+		inordor(p->left);
+		visit(p);
+		inordor(p->right);
+	}
+}
+
+/* 深度优先之后序遍历 */
+void BST::postordor() const
+{
+	postordor(root);
+}
+void BST::postordor(BSTNode *p) const
+{
+	if (nullptr  != p)
+	{
+		postordor(p->left);
+		postordor(p->right);
+		visit(p);
+	}
 }
