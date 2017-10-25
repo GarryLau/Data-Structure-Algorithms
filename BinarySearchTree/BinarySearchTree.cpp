7035bb331f961b4cc5794f3b6cebecca73918d4c
GarryLau <gan.lau@outlook.com>
2017-10-25 20:49:45 +0800

    Update BinarySearchTree.cpp

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -1,12 +1,16 @@
 #include "BinarySearchTree.h"
 #include <iostream>
+#include <stack>
+#include <queue>
 
 BST::BST()
 {
+	root = nullptr;
 }
 
 BST::~BST()
 {
+	clear();
 }
 
 /* 访问节点元素 */
@@ -17,7 +21,26 @@ void BST::visit(BSTNode *p) const
 /* 广度优先遍历 */
 void BST::breadthFirst() const
 {
-
+	if (nullptr != root)
+	{
+		std::queue<BSTNode *> trackQueue;
+		BSTNode *pNode = root;
+		trackQueue.push(pNode);
+		while (!trackQueue.empty())
+		{
+			pNode = trackQueue.front();
+			visit(pNode);
+			trackQueue.pop();
+			if (pNode->left != nullptr)
+			{
+				trackQueue.push(pNode->left);
+			}
+			if (pNode->right != nullptr)
+			{
+				trackQueue.push(pNode->right);
+			}
+		}
+	}
 }
 /* 深度优先之前序遍历-对外接口 */
 void BST::preordor() const
@@ -64,19 +87,51 @@ void BST::postordor(BSTNode *p) const
 		visit(p);
 	}
 }
-/* 查找-对外接口 */
-int * BST::search(const int &val) const
+/* 深度优先之前序遍历-非递归实现 */
+void BST::interactivePreordor() const
 {
+	if (nullptr != root)
+	{
+		std::stack<BSTNode*> traceStack;
+		BSTNode* pNode = root;
+		traceStack.push(root);
+		while (!traceStack.empty())
+		{
+			pNode = traceStack.top();
+			visit(pNode);
+			traceStack.pop();
+			if (nullptr != pNode->right)
+			{
+				traceStack.push(pNode->right);
+			}
+			if (nullptr != pNode->left)
+			{
+				traceStack.push(pNode->left);
+			}
+		}
+	}
 }
+/* 深度优先之中序遍历-非递归实现 */
+void BST::interactiveInordor() const
+{
+
+}
+/* 深度优先之后序遍历-非递归实现 */
+void BST::interactivePostordor() const
+{
+
+}
+
 /* 清空树-对外接口 */
 void BST::clear()
 {
-
+	clear(root);
+	root = nullptr;
 }
 /* 判断是否为空树 */
 bool BST::isEmpty() const
 {
-
+	return nullptr == root;
 }
 /* 插入 */
 void BST::insert(const int &val)
@@ -101,6 +156,11 @@ void BST::deleteByCopying(BSTNode *p)
 void BST::clear(BSTNode *p)
 {
 
+}
+/* 查找-对外接口 */
+int * BST::search(const int &val) const
+{
+	return search(root, val);
 }
 /* 查找-对内实现 */
 int * BST::search(BSTNode *p, const int &val) const
