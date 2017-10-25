8ed497878b4d84f56644b8f818b6ef00e7ebcb28
GarryLau <gan.lau@outlook.com>
2017-10-25 09:50:54 +0800

    Update BinarySearchTree.cpp

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -15,11 +15,12 @@ void BST::visit(BSTNode *p) const
 	std::cout << p->value << "  " << std::endl;
 }
 
-/* 深度优先之前序遍历 */
+/* 深度优先之前序遍历-对外接口 */
 void BST::preordor() const
 {
 	preordor(root);
 }
+/* 深度优先之前序遍历-递归实现 */
 void BST::preordor(BSTNode *p) const
 {
 	if (nullptr != p)
@@ -29,11 +30,12 @@ void BST::preordor(BSTNode *p) const
 		preordor(p->right);
 	}
 }
-/* 深度优先之中序遍历 */
+/* 深度优先之中序遍历-对外接口 */
 void BST::inordor() const
 {
 	inordor(root);
 }
+/* 深度优先之中序遍历-递归实现 */
 void BST::inordor(BSTNode *p) const
 {
 	if (nullptr != p)
@@ -44,11 +46,12 @@ void BST::inordor(BSTNode *p) const
 	}
 }
 
-/* 深度优先之后序遍历 */
+/* 深度优先之后序遍历-对外接口 */
 void BST::postordor() const
 {
 	postordor(root);
 }
+/* 深度优先之后序遍历-递归实现 */
 void BST::postordor(BSTNode *p) const
 {
 	if (nullptr  != p)
